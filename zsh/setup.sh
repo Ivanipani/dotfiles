45be4e04b90271d6e397bd3e46e6f8882ea2eb7f
Ivan Perdomo <iperdomo2016@gmail.com>
2026-03-13 21:17:07 -0400

    update zsh to use zplug

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -41,34 +41,4 @@ cmd_setup() {
   echo "Done."
 }
 
-cmd_install() {
-  ZSH="${ZSH:-$HOME/.oh-my-zsh}"
-
-  if [[ ! -d "$ZSH" ]]; then
-    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-  else
-    echo "Oh-My-Zsh is already installed"
-  fi
-
-  local plugins=(
-    "zsh-autosuggestions|https://github.com/zsh-users/zsh-autosuggestions.git"
-    "zsh-syntax-highlighting|https://github.com/zsh-users/zsh-syntax-highlighting.git"
-    "zsh-vi-mode|https://github.com/jeffreytse/zsh-vi-mode.git"
-  )
-
-  for entry in "${plugins[@]}"; do
-    local name="${entry%%|*}"
-    local url="${entry##*|}"
-    if [[ ! -d "$ZSH/plugins/$name" ]]; then
-      git clone "$url" "$ZSH/plugins/$name"
-    else
-      echo "$name is already installed"
-    fi
-  done
-}
-
-case "${1:-}" in
-  setup)   cmd_setup ;;
-  install) cmd_install ;;
-  *)       echo "Usage: $0 {setup|install}"; exit 1 ;;
-esac
+cmd_setup
