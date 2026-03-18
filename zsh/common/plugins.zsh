599529ea2864819da5803dd9f6458f7b5dab6935
Ivan Perdomo <iperdomo2016@gmail.com>
2026-03-17 22:56:07 -0400

    remove extra zsh plugin

--- zsh/common/plugins.zsh
+++ zsh/common/plugins.zsh
@@ -1,6 +1,5 @@
 source $ZPLUG_HOME/init.zsh
 
-zplug "robbyrussell/oh-my-zsh", use:"$OMZSH_USE"
 zplug "themes/gnzh", from:oh-my-zsh, as:theme
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
