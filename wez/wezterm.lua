f3c27d7efb6071bf90feedc57986d510e683f49f
Ivan Perdomo <iperdomo2016@gmail.com>
2026-04-30 22:57:09 -0400

    add zellij layout

--- wez/wezterm.lua
+++ wez/wezterm.lua
@@ -5,7 +5,8 @@ config.color_scheme = "Ayu Mirage"
 config.font = wezterm.font "Inconsolata Nerd Font"
 
 config.keys = {
-    {key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay}
+    {key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay},
+    {key = "p", mods = "CTRL|SHIFT", action = wezterm.action.SpawnCommandInNewWindow {}},
 }
 
 config.term = "xterm-256color"
