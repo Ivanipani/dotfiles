3b2ba6967d8ad2363a2cf06ea2828fef621a6f22
Ivan Perdomo <iperdomo2016@gmail.com>
2026-04-30 23:52:45 -0400

    fix DOTFILE_DIR path on linux

--- zsh/linux/main.zsh
+++ zsh/linux/main.zsh
@@ -4,7 +4,8 @@ export ZPLUG_HOME=$HOME/.zplug
 export XDG_CONFIG_HOME="$HOME/.config"
 ZSH_CONFIG_HOME="$XDG_CONFIG_HOME/zsh"
 
-export DOTFILE_DIR=$(realpath $HOME/.zshrc/../../..)
+# export DOTFILE_DIR=$(realpath $HOME/.zshrc/../../..)
+export DOTFILE_DIR=$(dirname "$(dirname "$(dirname "$(realpath "$HOME/.zshrc")")")")
 
 source $XDG_CONFIG_HOME/zsh/01-env.zsh
 source $XDG_CONFIG_HOME/zsh/02-aliases.zsh
