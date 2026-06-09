9746bde871194ebeb3181da8d54cef9d244d6428
Ivan Perdomo <iperdomo2016@gmail.com>
2026-06-09 00:01:11 -0400

    add gcloud CLI scripts

--- claude/CLAUDE.md
+++ claude/CLAUDE.md
@@ -2,3 +2,4 @@
 
 - NEVER use git commands. Prefer jujutsu for viewing commit history.
 - Avoid over-explaining technical decisions. Your comments should be brief and direct.
+- Follow the Don't Repeat Yourself principle when writing code. Avoid duplicating existing code.
--- zsh/mac/main.zsh
+++ zsh/mac/main.zsh
@@ -10,3 +10,9 @@ source $XDG_CONFIG_HOME/zsh/04-integrations.zsh
 source $XDG_CONFIG_HOME/zsh/05-options.zsh
 source $XDG_CONFIG_HOME/zsh/06-plugins.zsh
 
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/ivanperdomo/github/ivanipani/doghouse/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/ivanperdomo/github/ivanipani/doghouse/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/ivanperdomo/github/ivanipani/doghouse/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/ivanperdomo/github/ivanipani/doghouse/google-cloud-sdk/completion.zsh.inc'; fi
